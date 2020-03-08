a7c7081aeab53f4887897b1d7259f10aacc8609e
Fumiaki Kinoshita <fumiexcel@gmail.com>
2020-03-09 08:46:15 +0900

    tidy up

--- Coroutine.hs
+++ Coroutine.hs
@@ -7,18 +7,15 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE BangPatterns #-}
 module Coroutine where
 
 import GHC.Prim
 import GHC.IO (IO(..))
 import Data.Foldable
-import Control.Monad.IO.Class
-import Data.Functor
-import Control.Concurrent.MVar
 import Data.IORef
-import Control.Monad
+import Control.Monad.IO.Class
 import Control.Monad.Trans.Reader
-import System.IO.Unsafe
 
 unsafeReset :: IO a -> IO a
 unsafeReset (IO a) = IO (reset# a)
@@ -26,23 +23,26 @@ unsafeReset (IO a) = IO (reset# a)
 unsafeShift :: ((IO a -> IO b) -> IO b) -> IO a
 unsafeShift f = IO $ shift# $ \k -> case f (\(IO a) -> IO (k a)) of IO b -> b
 
-newtype Source m a = Source { unSource :: m (Either (a, Source m a) ()) }
+data Step a r = More !a !r | Done
+
+newtype Source m a = Source { unSource :: m (Step a (Source m a)) }
 
 emptySource :: Applicative m => Source m a
-emptySource = Source $ pure $ Right ()
+emptySource = Source $ pure Done
 
 type SourceBuilder a = IORef (Source IO a)
 
 loremIpsum :: ReaderT (SourceBuilder String) IO ()
-loremIpsum = for_ (zip [0..] $ words "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+loremIpsum = for_ (zip [0 :: Int ..] $ words "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
   $ \(i, str) -> do
     liftIO $ print i
     yield str
 
-yield :: a -> ReaderT (SourceBuilder String) IO ()
+yield :: a -> ReaderT (SourceBuilder a) IO ()
 yield x = ReaderT $ \r -> do
-  result <- unsafeShift $ \cont -> pure $ Left (x, Source $ unsafeReset $ cont $ pure $ Right ())
+  result <- unsafeShift $ \cont -> pure $ More x $ Source $ unsafeReset $ cont $ pure Done
   writeIORef r $ Source $ pure result
+{-# INLINE yield #-}
 
 runCoroutine :: ReaderT (SourceBuilder a) IO () -> Source IO a
 runCoroutine m = Source $ unsafeReset $ do
@@ -52,5 +52,17 @@ runCoroutine m = Source $ unsafeReset $ do
 
 printSource :: Show a => Source IO a -> IO ()
 printSource (Source m) = m >>= \case
-  Left (a, cont) -> print a >> printSource cont
-  Right () -> pure ()
+  More a cont -> print a >> printSource cont
+  Done -> pure ()
+
+sumSource :: Source IO Int -> IO Int
+sumSource = go 0 where
+  go !acc (Source m) = m >>= \case
+    More a cont -> go (acc + a) cont
+    Done -> pure acc
+{-# INLINE sumSource #-}
+
+drain :: Source IO a -> IO ()
+drain (Source m) = m >>= \case
+  More _ cont -> drain cont
+  Done -> pure ()
